d047d4a5a5fe75d133dfa43a41eb39ff9ae6d75e
root <root@maoshu.localdomain>
2022-08-30 09:05:41 +0800

    文件上传功能已完成

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+)
+
+func upload(ctx *gin.Context) {
+	form, err := ctx.MultipartForm()
+	if err != nil {
+		ctx.String(http.StatusBadRequest, "get form err: %s", err.Error())
+		return
+	}
+
+	files := form.File["files"]
+
+	for _, file := range files {
+		filename := filepath.Join("./uploadfile", filepath.Base(file.Filename))
+		log.Println(file.Filename)
+
+		if err := ctx.SaveUploadedFile(file, filename); err != nil {
+			ctx.String(http.StatusBadRequest, "upload file err: %s", err.Error())
+			return
+		}
+	}
+	ctx.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
+}
 
 func main() {
-	fmt.Println("welcome to you")
+	router := gin.Default()
+	router.MaxMultipartMemory = 8 << 20
+	router.StaticFile("/", "./public")
+	router.Static("/file", "./uploadfile")
+	router.POST("/upload", upload)
+
+	router.Run(":8000")
 }
